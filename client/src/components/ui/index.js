14152d958c78685422cbe46be8d0d571c0cfd919
Asher Morse <ashermorse@icloud.com>
2025-01-23 15:52:33 -0800

    fix

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -1,5 +1,5 @@
-export { default as Input } from './Input';
-export { default as Button } from './Button';
-export { default as Select } from './Select';
+export { default as Input } from './input';
+export { default as Button } from './button';
+export { default as Select } from './select';
 export { Checkbox } from './Checkbox';
 export { default as StarRating } from './StarRating';
